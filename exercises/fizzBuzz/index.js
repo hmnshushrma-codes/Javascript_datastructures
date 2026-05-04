9445e6edff9e930ef2a4aa12721d860240d2484d
himanshugiv <himanshu@givhealthcare.com>
2026-05-05 03:51:58 +0530

    fixx buzz problem

---
+++ exercises/fizzBuzz/index.js
@@ -0,0 +1,22 @@
+// fizzBUzz is a common coding problem that is often used to test a candidate's understanding of loops, conditionals, and basic programming logic.
+//  The problem is typically stated as follows:
+// Write a program that prints the numbers from 1 to n.
+// But for multiples of three print "Fizz" instead of the number
+// and for the multiples of five print "Buzz".
+// For numbers which are multiples of both three and five print "FizzBuzz".
+
+function fizzBuzz(n) {
+  for (let i = 1; i <= n; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("FizzBuzz");
+    } else if (i % 3 === 0) {
+      console.log("fizz");
+    } else if (i % 5 === 0) {
+      console.log("buzz");
+    } else {
+      console.log(i);
+    }
+  }
+}
+
+console.log(fizzBuzz(5))
